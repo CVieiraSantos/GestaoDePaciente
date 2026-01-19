10112d73aea4f04219c8d99177ac75bcfa63e0df
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-01-18 23:27:27 -0300

    Retirando comentario

--- GestaoDePaciente/Program.cs
+++ GestaoDePaciente/Program.cs
@@ -2,37 +2,10 @@ using GestaoDePaciente.Infrastructure;
 using Microsoft.OpenApi.Models;
 
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddInfraStructure(builder.Configuration);
-
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.MapControllers();
-
-//app.Run();
-
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers
 builder.Services.AddControllers();
 
-// Swagger (OBRIGATÓRIO)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -55,12 +28,10 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-// Infra
 builder.Services.AddInfraStructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
